a2c2ccb522701f3c313af0711d6d9cea031d2ce1
Ed Botzum <contact@edbotz.us>
2012-12-18 23:04:02 -0500

    Fixed the Routes

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 app.get('/4for4', routes.philly4);
-app.get('/xmas-2012', routes.philly4);
+app.get('/xmas-2012', routes.xmas2012);
 
 var port = process.env.PORT || 3000;
 
--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,13 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'edbotz - crafting code since 1998' })
+  res.render('index', { title: 'edbotz - crafting code since 1998' });
 };
 
 exports.philly4 = function(req, res){
-  res.render('4for4', { title: 'edbotz - 4 for 4' })
+  res.render('4for4', { title: 'edbotz - 4 for 4' });
 };
 
-exports.philly4 = function(req, res){
-  res.render('xmas-2012', { title: 'Merry Christmas & Happy New Year!', layout: false })
+exports.xmas2012 = function(req, res){
+  res.render('xmas-2012', { title: 'Merry Christmas & Happy New Year!', layout: false });
 };
